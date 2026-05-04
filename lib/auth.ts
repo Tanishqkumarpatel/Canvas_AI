1192e028442f5acd9780bb9d35c86483d27d2b67
Tanishqkumarpatel <tanishq38123@gmail.com>
2026-05-04 15:53:12 -0700

    fix: build

--- lib/auth.ts
+++ lib/auth.ts
@@ -11,5 +11,6 @@ export async function signup(formData: FormData) {
     const email = formData.get("email");
     const pwd = formData.get("password");
     console.log("Signining Up:", email, pwd);
+    
     return;
 }
