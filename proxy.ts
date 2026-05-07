8ff369fed1a1291acec60f978aaf7dd80cdeae5c
Tanishqkumarpatel <tanishq38123@gmail.com>
2026-05-06 20:16:29 -0700

    fixed proxy.ts for development

--- proxy.ts
+++ proxy.ts
@@ -1,4 +1,16 @@
-export {default} from 'next-auth/middleware'
+import { getToken } from 'next-auth/jwt'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function proxy(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/auth', req.url))
+  }
+
+  return NextResponse.next()
+}
+
 export const config = {
-    matcher: ['/dashboard/:path*']
+  matcher: ['/dashboard/:path*']
 }
