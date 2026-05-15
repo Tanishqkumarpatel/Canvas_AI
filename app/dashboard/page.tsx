080a325ee4013b8fd2fd7f8e21d5c5652b089335
Tanishqkumarpatel <tanishq38123@gmail.com>
2026-05-14 17:18:14 -0700

    added demo warning

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,10 +10,19 @@ export default async function Dashboard() {
         )
     } else {
         return (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-                {courses.map((course) => (
-                    <CourseCard course={course} key={course.id} />
-                ))}
+            <div className="flex flex-col gap-6">
+                <div className="bg-blue-50 border border-blue-100 p-4 rounded-2xl">
+                    <p className="text-sm text-blue-700 font-medium text-center">
+                        Viewing Demo Material. Submit your Canvas URL and Token in 
+                        <span className="font-bold"> Settings</span> to view your actual courses.
+                    </p>
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {courses.map((course) => (
+                        <CourseCard course={course} key={course.id} />
+                    ))}
+                </div>
             </div>
         )     
     }   
